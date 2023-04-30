8cc3bef63b9fc75cc7d86dcb682f3b73786ed7f6
Siddhant Prateek <siddhantprateek@gmail.com>
2023-04-30 22:57:38 +0530

    routing context added

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from './header/header.components';
 export { default as Footer } from './footer/footer.components';
 export { default as Hero } from './hero/hero.components';
+export { default as PropertyCard } from './shared/propertycard/propertycard.shared';
+export { default as OwnershipCard } from './shared/ownedpropertycard/ownedpropertycard.shared';
