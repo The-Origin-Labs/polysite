ca883e4ccc7623807e8653b252a42ca53042fb85
Siddhant Prateek <siddhantprateek@gmail.com>
2023-04-30 22:57:17 +0530

    shared components

--- src/components/shared/searchbox/searchbox.shared.jsx
+++ src/components/shared/searchbox/searchbox.shared.jsx
@@ -11,15 +11,15 @@ const Searchbox = () => {
       <div className="search-fields">
         <div className="search-act location">
           <h3>Location</h3>
-          <input type="text" placeholder='Enter Location / Area' id="" />
+          <input className='prop-search' type="text" placeholder='Enter Location/Area' id="" />
         </div>
         <div className="search-act state">
           <h3>State</h3>
-          <input type="text" placeholder='Enter State' id="" />
+          <input className='prop-search' type="text" placeholder='Enter State' id="" />
         </div>
         <div className="search-act pincode">
           <h3>Pin Code</h3>
-          <input type="text" placeholder='Enter Pin-Code' id="" />
+          <input className='prop-search' type="text" placeholder='Enter Pin-Code' id="" />
         </div>
         <button className="search-property">Search</button>
       </div>
