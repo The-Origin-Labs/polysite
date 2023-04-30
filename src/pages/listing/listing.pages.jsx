f70d9f0fcaca1a733329884770ab0b493dc4651b
Siddhant Prateek <siddhantprateek@gmail.com>
2023-04-30 22:57:27 +0530

    pages completed

--- src/pages/listing/listing.pages.jsx
+++ src/pages/listing/listing.pages.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { PropertyCard } from '../../components';
+import './listing.styles.css';
+import Searchbox from '../../components/shared/searchbox/searchbox.shared';
+
 
 const Listing = () => {
   return (
     <div className='listing-page'>
-        Listing
+      <div>
+        <div className="listing-search-header">
+          <Searchbox />
+        </div>
+        <div className="listed-properties">
+          {[...Array(10)].map((key) => (
+            <PropertyCard />
+          ))}
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/pages/profile/profile.pages.jsx
+++ src/pages/profile/profile.pages.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { OwnershipCard } from '../../components';
+import './profile.styles.css';
 const Profile = () => {
   const { profileid } = useParams();
   return (
     <div className='profile-page'>
-        {profileid}
+      <div>
+        <div className="profile-header">
+          <img className='profile-image' src={"https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"} alt="" />
+          <div className="profile-content-container">
+            <div className="user-header-info">
+              <h1>{profileid}</h1>
+              <p>0xFcf51wa...........saGa</p>
+              <button>Copy</button>
+            </div>
+            <div className='user-other-info'>
+              <div className="age-info info">
+                <h1>34</h1>
+                <p>Age</p>
+              </div>
+              <div className="marital-info info">
+                <h1>Married</h1>
+                <p>Marital Status</p>
+              </div>
+              <div className="profession-info info">
+                <h1>Attorney</h1>
+                <p>Profession</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Properties Owned */}
+        <div className="owned-properties">
+          <OwnershipCard/>  
+          <OwnershipCard/>  
+          <OwnershipCard/>  
+        </div>
+      </div>        
     </div>
   )
 }
