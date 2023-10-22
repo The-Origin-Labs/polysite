639c907d615dac8b037eebf3960b01f78ea6df00
siddhantprateek <siddhantprateek@gmail.com>
2023-10-22 17:44:13 +0530

    ft(assets) updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,7 @@
 export { default as LOGO } from './logo.svg';
 export { default as HEROIMG } from './heropol.svg';
 export { default as GITHUB } from './github.png';
+export { default as SEARCHICON } from './search-icon.gif';
+export { default as INSTAGRAM } from './insta.png';
+export { default as LINKEDIN } from './linkedin.png';
+export { default as TWITTER } from './twitter.png';
